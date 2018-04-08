2b9c06bd11a1c935e00cf5bf8e83e3ecfe9eb42b
Michael Bishop <miklb@miklb.com>
2018-04-08 02:56:28 -0400

    chore: remove tracking code

--- footer.php
+++ footer.php
@@ -9,19 +9,5 @@
   </div>
 	<?php wp_footer(); ?>
 </footer>
-<script type="text/javascript">
-  var _gauges = _gauges || [];
-  (function() {
-    var t   = document.createElement('script');
-    t.type  = 'text/javascript';
-    t.async = true;
-    t.id    = 'gauges-tracker';
-    t.setAttribute('data-site-id', '5988c6ac4b9eb70bec143627');
-    t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
-    t.src = 'https://d36ee2fcip1434.cloudfront.net/track.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(t, s);
-  })();
-</script>
 </body>
 </html>
