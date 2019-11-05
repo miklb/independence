8c39f918c9381e0710b64eca006544fd10339e88
Michael Bishop <miklb@miklb.com>
2019-11-05 18:30:18 -0500

    fix: use lowercase for classes

--- template-parts/content.php
+++ template-parts/content.php
@@ -3,7 +3,7 @@
 	<header class="entry-header">
 		<?php
 		if ( is_single() ) :
-			the_title( '<h1 class="entry-title p-name title is-2">', '</h1>' );
+			the_title( '<h1 Class="entry-title p-name title is-2">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title p-name title is-2"><a class="u-url" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
