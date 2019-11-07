20c14664bf201ddf0ca110e48cf6f845796c9b0d
Michael Bishop <miklb@miklb.com>
2019-11-07 11:22:53 -0500

    fix: pages still need a loop

--- page.php
+++ page.php
@@ -24,16 +24,11 @@ get_header(); ?>
 		the_title( '<h1 class="entry-title p-name title is-2">', '</h1>' );
 		?>
 		<div class="content is-medium content-wrapper">
+		<?php while ( have_posts() ) : the_post(); ?>
 		<?php
-			the_content(
-				sprintf(
-					/* translators: %s: Name of current post. */
-					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'independence' ), array( 'span' => array( 'class' => array() ) ) ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				) 
-			);
-
-			?>
+			the_content();
+		?>
+		<?php endwhile; // end of the loop. ?>
 
 		</div><!-- .content-wrapper -->
 	</section>
